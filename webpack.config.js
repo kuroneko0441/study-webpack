379e642f2082f4367e0dadda9faa38fbd2bb3093
JoonYoung <kuroneko0441@gmail.com>
2020-11-21 00:43:54 +0900

    Babel 및 babel-loader 추가
    - tsc 대상이 es2015로 설정되어 있으므로
      es2015 플러그인만 추가함

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,42 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
+const babelPlugins = {
+  /**
+   * ES2015 Plugins
+   * {@link https://babeljs.io/docs/en/plugins#es2015}
+   */
+  es2015: [
+    '@babel/plugin-transform-arrow-functions',
+    '@babel/plugin-transform-block-scoped-functions',
+    '@babel/plugin-transform-block-scoping',
+    '@babel/plugin-transform-classes',
+    '@babel/plugin-transform-computed-properties',
+    '@babel/plugin-transform-destructuring',
+    '@babel/plugin-transform-duplicate-keys',
+    '@babel/plugin-transform-for-of',
+    '@babel/plugin-transform-function-name',
+    '@babel/plugin-transform-instanceof',
+    '@babel/plugin-transform-literals',
+    '@babel/plugin-transform-new-target',
+    '@babel/plugin-transform-object-super',
+    '@babel/plugin-transform-parameters',
+    '@babel/plugin-transform-shorthand-properties',
+    '@babel/plugin-transform-spread',
+    '@babel/plugin-transform-sticky-regex',
+    '@babel/plugin-transform-template-literals',
+    '@babel/plugin-transform-typeof-symbol',
+    '@babel/plugin-transform-unicode-escapes',
+    '@babel/plugin-transform-unicode-regex',
+  ],
+};
+
+const babelOptions = {
+  plugins: [
+    ...babelPlugins.es2015,
+  ],
+};
+
 module.exports = {
   mode: 'development',
   entry: {
@@ -17,6 +53,10 @@ module.exports = {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
         use: [
+          {
+            loader: 'babel-loader',
+            options: babelOptions,
+          },
           {
             loader: 'ts-loader',
           },
