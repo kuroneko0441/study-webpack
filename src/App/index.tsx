e72a4162c9212a30299f5b3dd309eae911fe14d2
JoonYoung Lee <kuroneko0441@gmail.com>
2020-11-21 23:57:33 +0900

    React props 추가

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -2,8 +2,17 @@ import { printMe } from 'print';
 import * as React from 'react';
 import './style.scss';
 
-export class App extends React.Component<unknown, unknown> {
+interface Props {
+  name?: string;
+}
+
+export class App extends React.Component<Props, unknown> {
+
+  public constructor(props: Props) {
+    super(props);
+  }
+
   public render(): React.ReactNode {
-    return <h1 onClick={printMe}>Hello React!</h1>;
+    return <h1 onClick={printMe}>Hello {this.props.name || 'stranger'}!</h1>;
   }
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import * as ReactDOM from 'react-dom';
 import { App } from './App';
 
 ReactDOM.render(
-  <App />,
+  <App name={'React'} />,
   document.getElementById('root'),
 );
