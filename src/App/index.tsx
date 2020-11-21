886b9c4879a9ebd943e321f6bbed31e7c6da121f
JoonYoung Lee <kuroneko0441@gmail.com>
2020-11-22 00:04:53 +0900

    React state 추가

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -6,13 +6,31 @@ interface Props {
   name?: string;
 }
 
-export class App extends React.Component<Props, unknown> {
+interface State {
+  clickCount: number;
+}
+
+export class App extends React.Component<Props, State> {
 
   public constructor(props: Props) {
     super(props);
+
+    this.state = {
+      clickCount: 0,
+    };
+
+    this.onClick = this.onClick.bind(this);
   }
 
   public render(): React.ReactNode {
-    return <h1 onClick={printMe}>Hello {this.props.name || 'stranger'}!</h1>;
+    return <h1 onClick={this.onClick}>Hello {this.props.name || 'stranger'}! Clicked {this.state.clickCount} times!</h1>;
+  }
+
+  private onClick(): void {
+    printMe();
+    this.setState({
+      ...this.state,
+      clickCount: this.state.clickCount + 1,
+    });
   }
 }
